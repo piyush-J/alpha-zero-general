6ca6fe6afaa34259bc4af51541e825877c890acd
Piyush Jha <piyush.jha@uwaterloo.ca>
2022-12-18 19:13:35 -0500

    add compute canada scripts

---
+++ create_env.sh
@@ -0,0 +1,7 @@
+module load cuda gcc python/3.7
+
+virtualenv --no-download --clear ~/mcts
+
+source ~/mcts/bin/activate
+
+pip install -r requirements.txt
---
+++ run_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M        # memory per node
+#SBATCH --account=def-vganesh
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=piyush.jha@uwaterloo.ca
+#SBATCH --time=0-24:00      # time (DD-HH:MM)
+#SBATCH --output=exptResults/run-%N-%j.out  # %N for node name, %j for jobID
+
+# Define a timestamp function
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S" # current time
+}
+
+OUT_FOLD="exptResults/$(timestamp)"
+mkdir -p "$OUT_FOLD"
+echo "Writing output to folder $OUT_FOLD"
+
+module load cuda gcc python/3.7
+
+source ~/mcts/bin/activate
+
+python ./main.py \
+  > "$OUT_FOLD/out.txt" 2>&1
